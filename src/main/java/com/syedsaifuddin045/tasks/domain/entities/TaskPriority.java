93332256288e16f30f8487662dfcf6b2867c160f
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2025-06-16 23:05:19 +0530

    Setup Entities

---
+++ src/main/java/com/syedsaifuddin045/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.syedsaifuddin045.tasks.domain.entities;
+
+public enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/syedsaifuddin045/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.syedsaifuddin045.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN,
+    CLOSED
+}
