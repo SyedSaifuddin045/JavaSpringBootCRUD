42bfe91dcb385de7391cc709f3fd71a452604778
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2025-06-16 22:14:11 +0530

    Initial Project Initialized

---
+++ src/test/java/com/syedsaifuddin045/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syedsaifuddin045.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
