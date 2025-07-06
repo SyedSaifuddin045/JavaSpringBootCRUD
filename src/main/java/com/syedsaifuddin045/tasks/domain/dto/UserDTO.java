4504b77ac156e464685affead76fd6fb999104fa
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2025-07-06 20:33:44 +0530

    Added Ignore to User Passwords

--- src/main/java/com/syedsaifuddin045/tasks/domain/dto/UserDTO.java
+++ src/main/java/com/syedsaifuddin045/tasks/domain/dto/UserDTO.java
@@ -2,10 +2,13 @@ package com.syedsaifuddin045.tasks.domain.dto;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UserDTO(
         UUID id,
         String name,
         String email,
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String password,
         byte[] profilePicture) {
 }
